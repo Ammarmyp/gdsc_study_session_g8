c1c52d3cb7b985ab71e4e0ef79ede970abdf6cb1
Ammarmyp <ammarmyp@gmail.com>
2023-11-29 13:20:53 +0300

    calculating BMI

---
+++ bmi.js
@@ -0,0 +1,28 @@
+// var age =25;
+// console.log(age);
+
+// //hoisting
+// if (age <100) {
+//     var no =40;
+//     console.log(age);
+//     console.log(no);
+// }
+// console.log(no * 2);
+// console.log(no /2 );
+// let result = true && false;
+// let result1 = true || false;
+// let result2 = !true;
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+
+// let x = 2;
+//  x = 4;
+// console.log(x);
+
+const mass = prompt ("Enter mass");
+const height = prompt ("Enter height");
+
+const BMI = mass / (height **2);
+
+console.log("your BMI is : " + BMI)

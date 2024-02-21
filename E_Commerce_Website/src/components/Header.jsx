c21072c02556148f8ac9f39a16a6b13ee3de7dcd
Ammarmyp <ammarmyp@gmail.com>
2024-02-21 15:42:29 +0300

    Header and Nav bar UI set up

--- E_Commerce_Website/src/components/Header.jsx
+++ E_Commerce_Website/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Header() {
   return (
     <>
-      <div></div>
+      <nav className="flex justify-between  bg-zinc-400 h-14 p-4">
+        <h1>Header</h1>
+        <FaCartShopping onClick={() => console.log("cart clicked")} />
+      </nav>
     </>
   );
 }

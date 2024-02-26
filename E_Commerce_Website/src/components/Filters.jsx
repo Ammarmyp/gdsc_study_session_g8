498119cc572f23b335eafe5455fa498ecabddec8
Ammarmyp <ammarmyp@gmail.com>
2024-02-26 10:37:55 +0300

    fething the categories and items from fake store api

--- E_Commerce_Website/src/components/Filters.jsx
+++ E_Commerce_Website/src/components/Filters.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import items from '../data/services/items'
 
 const Filters = () => {
   return (
-    <div>Filters</div>
+    <ul>
+      {items.map((item)=> <li key={item.id}>{item.na}</li>)}
+    </ul>
   )
 }
 

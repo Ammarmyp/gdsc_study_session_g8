55bd8718e30f595461dd6236ee0a2a6105994212
Ammarmyp <ammarmyp@gmail.com>
2024-02-28 15:15:03 +0300

    design the product card

--- E_Commerce_Website/src/components/ProductCard.jsx
+++ E_Commerce_Website/src/components/ProductCard.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const ProductCard = ({ title, image }) => {
   return (
-    <div className="bg-gray-300 sm:w-[400px] w-[300px] h-[320px] flex justify-center items-center  mx-3 p-2 my-4 rounded-xl">
-      <div className="w-full h-full rounded-2xl bg-center bg-cover overflow-hidden transition-all duration-300 p-2">
+    <div className="bg-slate-100 border-solid border-[2px] border-slate-200 sm:w-[300px] w-[270px] h-[320px]  justify-center items-center  mx-2 p-[2px] my-4 rounded-xl">
+      <div className="w-full h-[230px] rounded-3xl bg-center bg-cover overflow-hidden transition-all duration-300 mb-2">
         <img
           src={image}
-          className="w-full h-full items-center mx-auto rounded object-center object-cover duration-700 transition-all"
+          className="w-full h-full items-center  rounded-2xl object-center object-cover duration-700 transition-all p-1"
         />
       </div>
-      <h3 className="text-left">{title}</h3>
-      <h3>Cart</h3>
+      <div className=" flex justify-between items-center mx-1 mt-2 p-2 text-sm font-medium">
+        <h3 className="w-[210px] text-left">{title}</h3>
+        <h3>Cart</h3>
+      </div>
     </div>
   );
 };

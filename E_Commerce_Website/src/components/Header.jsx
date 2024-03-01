2e210201ed70e6218d6aa7bd7a17e6cd8fd08e2e
Ammarmyp <ammarmyp@gmail.com>
2024-03-01 21:11:12 +0300

    slider margin

--- E_Commerce_Website/src/components/Header.jsx
+++ E_Commerce_Website/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Cart from "./Cart";
 function Header() {
   return (
     <>
-      <div className="fixed top-0 right-0 left-0 z-50 flex justify-between  bg-zinc-400 h-14 p-4 mb-5 mt-16">
+      <div className="fixed top-0 right-0 left-0 z-50 flex justify-between  bg-zinc-400 h-14 p-4 mb-5">
         <nav >
           <h1>Header</h1>
           {/* Move the cart to an indivitual component */}
